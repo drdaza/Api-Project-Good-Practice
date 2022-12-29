6eeb01241f867dc17d520a2e59713eeffca2e179
David Daza <drdaza18@gmail.com>
2022-12-29 18:53:05 +0100

    render space favorite store

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,9 +1,30 @@
+<script setup>
+import CardCharacter from '../components/CardComponent/CardCharacter.vue';
+import { FavCharacterStore } from '../stores/FavoriteStores';
+const favoriteStore = FavCharacterStore();
+
+</script>
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about" >
+    <div class="card-space" v-for="item of favoriteStore.FavCharacters">
+    <CardCharacter
+      :character="item"
+    />
+    </div>
   </div>
 </template>
 
-<style>
+<style lang="scss" scoped>
+@use '../assets/scss/main' as *;
 
+.about{
+  @include FlexDisplay(row, space-evenly, center, 100%, auto);
+  flex-wrap: wrap;
+  .card-space{
+    @include FlexDisplay(column, center, center, 23%, 60vh);
+    margin-top: 2%;
+    border: $terthiary-color 2px solid;
+    
+  }
+}
 </style>
